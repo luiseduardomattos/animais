bd9d2696b5b170d80bfbf338d4f25ec48e7d0a69
mattosluiseduardo <113482483+mattosluiseduardo@users.noreply.github.com>
2023-07-31 21:53:00 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace animais
+{
+    partial class animais
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxEspecie = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxIdade = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxSexo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPorte = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxComportamento = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxSobrepet = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxCidade = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxEstado = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBoxIdong = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBoxIdprotetor = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(45, 158);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(612, 26);
+            this.textBoxNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Especie";
+            // 
+            // textBoxEspecie
+            // 
+            this.textBoxEspecie.Location = new System.Drawing.Point(40, 307);
+            this.textBoxEspecie.Name = "textBoxEspecie";
+            this.textBoxEspecie.Size = new System.Drawing.Size(100, 26);
+            this.textBoxEspecie.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(295, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Idade";
+            // 
+            // textBoxIdade
+            // 
+            this.textBoxIdade.Location = new System.Drawing.Point(299, 307);
+            this.textBoxIdade.Name = "textBoxIdade";
+            this.textBoxIdade.Size = new System.Drawing.Size(100, 26);
+            this.textBoxIdade.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(505, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sexo";
+            // 
+            // textBoxSexo
+            // 
+            this.textBoxSexo.Location = new System.Drawing.Point(557, 307);
+            this.textBoxSexo.Name = "textBoxSexo";
+            this.textBoxSexo.Size = new System.Drawing.Size(100, 26);
+            this.textBoxSexo.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 394);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Porte";
+            // 
+            // textBoxPorte
+            // 
+            this.textBoxPorte.Location = new System.Drawing.Point(50, 490);
+            this.textBoxPorte.Name = "textBoxPorte";
+            this.textBoxPorte.Size = new System.Drawing.Size(100, 26);
+            this.textBoxPorte.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 404);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Comportamento";
+            // 
+            // textBoxComportamento
+            // 
+            this.textBoxComportamento.Location = new System.Drawing.Point(299, 490);
+            this.textBoxComportamento.Name = "textBoxComportamento";
+            this.textBoxComportamento.Size = new System.Drawing.Size(100, 26);
+            this.textBoxComportamento.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 706);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Sobre_pet";
+            // 
+            // textBoxSobrepet
+            // 
+            this.textBoxSobrepet.Location = new System.Drawing.Point(40, 754);
+            this.textBoxSobrepet.Multiline = true;
+            this.textBoxSobrepet.Name = "textBoxSobrepet";
+            this.textBoxSobrepet.Size = new System.Drawing.Size(632, 126);
+            this.textBoxSobrepet.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(45, 558);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Cidade";
+            // 
+            // textBoxCidade
+            // 
+            this.textBoxCidade.Location = new System.Drawing.Point(50, 625);
+            this.textBoxCidade.Name = "textBoxCidade";
+            this.textBoxCidade.Size = new System.Drawing.Size(100, 26);
+            this.textBoxCidade.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(295, 558);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 20);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Estado";
+            // 
+            // textBoxEstado
+            // 
+            this.textBoxEstado.Location = new System.Drawing.Point(299, 625);
+            this.textBoxEstado.Name = "textBoxEstado";
+            this.textBoxEstado.Size = new System.Drawing.Size(100, 26);
+            this.textBoxEstado.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(36, 28);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 20);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Animais";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(533, 943);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 32);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "SALVAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(553, 404);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(62, 20);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Id_Ong";
+            // 
+            // textBoxIdong
+            // 
+            this.textBoxIdong.Location = new System.Drawing.Point(557, 490);
+            this.textBoxIdong.Name = "textBoxIdong";
+            this.textBoxIdong.Size = new System.Drawing.Size(100, 26);
+            this.textBoxIdong.TabIndex = 21;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(553, 558);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(89, 20);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Id_Protetor";
+            // 
+            // textBoxIdprotetor
+            // 
+            this.textBoxIdprotetor.Location = new System.Drawing.Point(557, 625);
+            this.textBoxIdprotetor.Name = "textBoxIdprotetor";
+            this.textBoxIdprotetor.Size = new System.Drawing.Size(100, 26);
+            this.textBoxIdprotetor.TabIndex = 23;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 935);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 40);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "LIMPAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // animais
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(699, 1014);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxIdprotetor);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBoxIdong);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxEstado);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxCidade);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxSobrepet);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxComportamento);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxPorte);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxSexo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxIdade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxEspecie);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "animais";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxEspecie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxIdade;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxSexo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPorte;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxComportamento;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxSobrepet;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxCidade;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxEstado;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBoxIdong;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBoxIdprotetor;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+
+namespace animais
+{ 
+    public partial class animais: Form
+    {
+        public animais()
+        {
+            InitializeComponent();
+        }
+
+
+            private void animais_Load(object sender, EventArgs e)
+            {
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                MySqlConnection conn1 = new MySqlConnection("server=localhost:81, 127.0.0.1;username=root;database=adotapet1;password=");
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO animais (Nome, Especie, idade, Sexo, Porte, Comportamento, Sobre_Pet, Cidade, Estado, Id_Ong, Id_Protetor) VALUES (@Nome, @Especie, @Idade, @Sexo, @Porte, @Comportamento, @Sobrepet, @Cidade, @Estado, @Id_Ong, @Id_Protetor);", conn1);
+                conn1.Open();
+                
+
+                cmd.Parameters.AddWithValue("@Nome", textBoxNome.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+                cmd.Parameters.AddWithValue("@Especie", textBoxEspecie.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+                cmd.Parameters.AddWithValue("@Idade", textBoxIdade.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+
+                cmd.Parameters.AddWithValue("@Sexo", textBoxSexo.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+                cmd.Parameters.AddWithValue("@Porte", textBoxPorte.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+                cmd.Parameters.AddWithValue("@Comportamento", textBoxComportamento.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+                cmd.Parameters.AddWithValue("@Sobrepet", textBoxSobrepet.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+                cmd.Parameters.AddWithValue("@Cidade", textBoxCidade.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+                cmd.Parameters.AddWithValue("@Estado", textBoxEstado.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+                cmd.Parameters.AddWithValue("@Id_Ong", textBoxIdong.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+                cmd.Parameters.AddWithValue("@Id_Protetor", textBoxIdprotetor.Text);
+                if (textBoxNome.Text == "")
+                {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+                }
+
+
+
+
+
+                int rowAffected = cmd.ExecuteNonQuery();
+                conn1.Close();
+
+                if (rowAffected > 0)
+                {
+                    MessageBox.Show("Dados inseridos com sucesso!");
+                }
+                else
+                {
+                    MessageBox.Show("Falha ao inserir dados!");
+                }
+
+
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxNome.Clear();
+            Email.Clear();
+            Cpf.Clear();
+            textBoxTelefone.Clear();
+            textBoxEndereco.Clear();
+            textBoxBairro.Clear();
+            textBoxCidade.Clear();
+            textBoxEstado.Clear();
+            textBoxSenha.Clear();
+        }
+    }
+}
+    
+
